2df6c21cceeeab6ec7bb1c53db4e5814743d420e
Pierre Laporte <pierre@pingtimeout.fr>
2024-01-24 14:20:50 +0100

    Print API key during account creation

--- backend/__tests__/cli/test-admin-create-user.sh
+++ backend/__tests__/cli/test-admin-create-user.sh
@@ -23,8 +23,6 @@ set -eu
 EMAIL_ADDRESS=$1
 PASSWORD=${2:-$(uuidgen | tr '[:upper:]' '[:lower:]' | tr -d - | head -c 8)}
 
-echo Creating account for $EMAIL_ADDRESS with password: $PASSWORD
-
 USERNAME=$(
   aws --profile performance cognito-idp admin-create-user \
     --user-pool-id $USER_POOL_ID \
@@ -33,16 +31,29 @@ USERNAME=$(
     --message-action SUPPRESS \
   | jq -r .User.Username
 )
+echo -e "\033[32mPASS:\033[0m Created account for $EMAIL_ADDRESS (sub: $USERNAME) with password: $PASSWORD"
 
 aws --profile performance cognito-idp admin-set-user-password \
   --user-pool-id $USER_POOL_ID \
   --username "$EMAIL_ADDRESS" \
   --password "$PASSWORD" \
   --permanent
+echo -e "\033[32mPASS:\033[0m Confirmed account"
 
 echo "{\"userPoolId\": \"$USER_POOL_ID\", \"userName\": \"$USERNAME\"}" >/tmp/payload.json
 aws --profile performance lambda invoke \
   --function-name $POST_CONFIRMATION_LAMBDA_NAME \
   --payload file:///tmp/payload.json \
   /tmp/outputfile.txt \
-  --cli-binary-format raw-in-base64-out
+  --cli-binary-format raw-in-base64-out \
+  >/dev/null
+echo -e "\033[32mPASS:\033[0m Associated orgId and created API key"
+
+API_KEY=$(
+  aws --profile performance apigateway get-api-keys \
+    --name-query "$USERNAME" \
+    --include-value \
+  | jq -r '.items[0].value'
+)
+echo -e "\033[32mPASS:\033[0m Fetched API key"
+echo "export API_KEY=$API_KEY"
