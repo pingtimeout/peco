b8b97c87918f366ff128df5d8ee244bb05248f40
Pierre Laporte <pierre@pingtimeout.fr>
2024-01-24 14:21:29 +0100

    Allow optional description in Environment

--- backend/src/model/Environment.ts
+++ backend/src/model/Environment.ts
@@ -60,7 +60,7 @@ export class Environment {
         attrs["orgId"].S!,
         attrs["id"].S!,
         attrs["name"].S,
-        attrs["description"].S,
+        attrs["description"]?.S,
         attrs["tags"].L?.map((tag) => new Tag(tag.M!.name.S!, tag.M!.value.S!))
       );
     }
