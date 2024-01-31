82de36a20008671e31d3fd9034f244ae53d53d66
Pierre Laporte <pierre@pingtimeout.fr>
2024-01-31 21:30:19 +0100

    Throw error instead of string

--- backend/src/environment-variables.ts
+++ backend/src/environment-variables.ts
@@ -1,7 +1,7 @@
 function getEnv(name: string): string {
   const val = process.env[name];
   if (val === undefined || val === null) {
-    throw "Missing environment variable " + name;
+    throw new Error("Missing environment variable " + name);
   }
   return val;
 }
