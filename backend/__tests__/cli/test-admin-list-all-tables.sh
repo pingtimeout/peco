ee574307b6628917d1a26db712e942a45d072983
Pierre Laporte <pierre@pingtimeout.fr>
2024-01-31 22:19:19 +0100

    Use more compact output in debug script

--- backend/__tests__/cli/test-admin-list-all-tables.sh
+++ backend/__tests__/cli/test-admin-list-all-tables.sh
@@ -11,33 +11,33 @@ handle_error() {
 trap 'handle_error $LINENO' ERR
 
 echo "Use cases:"
-aws --profile performance dynamodb scan --table-name UseCases | jq .Items
+aws --profile performance dynamodb scan --table-name UseCases | jq .Items -c | underscore pretty
 echo
 
 echo "Environments:"
-aws --profile performance dynamodb scan --table-name Environments | jq .Items
+aws --profile performance dynamodb scan --table-name Environments | jq .Items -c | underscore pretty
 echo
 
 echo "Products:"
-aws --profile performance dynamodb scan --table-name Products | jq .Items
+aws --profile performance dynamodb scan --table-name Products | jq .Items -c | underscore pretty
 echo
 
 echo "Metric definitions:"
-aws --profile performance dynamodb scan --table-name MetricDefinitions | jq .Items
+aws --profile performance dynamodb scan --table-name MetricDefinitions | jq .Items -c | underscore pretty
 echo
 
 echo "Benchmark definitions:"
-aws --profile performance dynamodb scan --table-name BenchmarkDefinitions | jq .Items
+aws --profile performance dynamodb scan --table-name BenchmarkDefinitions | jq .Items -c | underscore pretty
 echo
 
 echo "Monitored metrics:"
-aws --profile performance dynamodb scan --table-name MonitoredMetrics | jq .Items
+aws --profile performance dynamodb scan --table-name MonitoredMetrics | jq .Items -c | underscore pretty
 echo
 
 echo "Benchmark runs:"
-aws --profile performance dynamodb scan --table-name BenchmarkRuns | jq .Items
+aws --profile performance dynamodb scan --table-name BenchmarkRuns | jq .Items -c | underscore pretty
 echo
 
 echo "Benchmark values:"
-aws --profile performance dynamodb scan --table-name BenchmarkValues | jq .Items
+aws --profile performance dynamodb scan --table-name BenchmarkValues | jq .Items -c | underscore pretty
 echo
