bbb54f68339a1de0d351815155af9027cf43a800
Pierre Laporte <pierre@pingtimeout.fr>
2024-01-31 16:53:19 +0100

    Add test script to list all tables

---
+++ backend/__tests__/cli/test-admin-list-all-tables.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+set -eu
+
+handle_error() {
+    echo
+    echo -e "\033[1m\033[41mAN ERROR OCCURRED ON LINE $1\033[0m" >&2
+    exit 1
+}
+
+trap 'handle_error $LINENO' ERR
+
+echo "Use cases:"
+aws --profile performance dynamodb scan --table-name UseCases | jq .Items
+echo
+
+echo "Environments:"
+aws --profile performance dynamodb scan --table-name Environments | jq .Items
+echo
+
+echo "Products:"
+aws --profile performance dynamodb scan --table-name Products | jq .Items
+echo
+
+echo "Metric definitions:"
+aws --profile performance dynamodb scan --table-name MetricDefinitions | jq .Items
+echo
+
+echo "Benchmark definitions:"
+aws --profile performance dynamodb scan --table-name BenchmarkDefinitions | jq .Items
+echo
+
+echo "Monitored metrics:"
+aws --profile performance dynamodb scan --table-name MonitoredMetrics | jq .Items
+echo
+
+echo "Benchmark runs:"
+aws --profile performance dynamodb scan --table-name BenchmarkRuns | jq .Items
+echo
+
+echo "Benchmark values:"
+aws --profile performance dynamodb scan --table-name BenchmarkValues | jq .Items
+echo
